329e5ffbaa4384c55a0ca8da2fb64ba61456b89d
matique <dittmar.krall@matique.de>
2019-03-03 13:21:52 +0100

    'a new try'

--- test/null_test.rb
+++ test/null_test.rb
@@ -7,9 +7,9 @@ describe 'No Flags' do
     Order.add_to_flags
   end
 
-  it 'respond to ActWithFlags API' do
+  it 'ActWithFlags API' do
     assert Order.respond_to?(:add_to_flags)
-    assert Order.respond_to?(:delete_from_flags)
+    assert Order.respond_to?(:remove_from_flags)
     assert Order.respond_to?(:clear_flags_at_save)
     assert Order.respond_to?(:act_with_flags)
   end
