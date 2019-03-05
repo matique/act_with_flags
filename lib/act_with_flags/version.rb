ce4168b2594ed75e024d8780d8f4e0402065f399
matique <dittmar.krall@matique.de>
2019-03-05 12:48:31 +0100

    'v0.0.5'

--- lib/act_with_flags/version.rb
+++ lib/act_with_flags/version.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module ActWithFlags
-  VERSION = '0.0.4'     # 2019-03-03
+  VERSION = '0.0.5'     # 2019-03-05
+#  VERSION = '0.0.4'     # 2019-03-03
 #  VERSION = '0.0.3'     # 2019-03-01
 #  VERSION = '0.0.2'     # 2019-02-28
 #  VERSION = '0.0.1'     # 2019-02-24
