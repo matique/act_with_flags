f080dae6bac21d0638ee7a89c25ed1d1fd4b4c07
matique <dittmar.krall@matique.de>
2019-04-03 13:05:41 +0200

    'added test inheritance flags_...'

---
+++ test/inheritance3_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class A < Order
+  add_to_flags y: 2
+end
+
+describe 'inheritance' do
+  let(:a) { A.create }
+
+  def setup
+    reset_order
+    Order.add_to_flags x: 1
+  end
+
+  it 'consistency' do
+    assert a.respond_to?(:x)
+    assert a.respond_to?(:y)
+    assert_equal false, a.x
+    assert_equal false, a.y
+    a.y = true
+    assert_equal true, a.y
+  end
+
+  it 'checks any?' do
+    a.x = true
+    assert a.flags_any?(:x, :y)
+    a.x = false
+    refute a.flags_any?(:x, :y)
+  end
+
+  it 'checks any? #2' do
+    a.y = true
+    assert a.flags_any?(:x, :y)
+    a.y = false
+    refute a.flags_any?(:x, :y)
+  end
+
+  it 'checks all?' do
+    a.x = a.y = true
+    assert a.flags_all?(:x, :y)
+    a.x = false
+    refute a.flags_all?(:x, :y)
+  end
+
+  it 'checks none? #2' do
+    a.x = a.y = true
+    refute a.flags_none?(:x, :y)
+    a.x = false
+    refute a.flags_none?(:x, :y)
+    a.y = false
+    assert a.flags_none?(:x, :y)
+  end
+
+end
