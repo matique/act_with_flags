3fd127216366e34a2b588c5cbf9cc0f7aaf21356
matique <dittmar.krall@matique.de>
2020-07-14 19:39:31 +0200

    'v0.2.3'

--- lib/act_with_flags/version.rb
+++ lib/act_with_flags/version.rb
@@ -2,7 +2,8 @@
 # rubocop: disable all
 
 module ActWithFlags
-  VERSION = '0.2.2'     # 2020-04-27
+  VERSION = '0.2.3'     # 2020-07-14
+# VERSION = '0.2.2'     # 2020-04-27
 # VERSION = '0.2.1'     # 2020-03-01
 # VERSION = '0.2.0'     # 2019-10-04
 # VERSION = '0.1.0'     # 2019-04-07
