251c082a4279120b17fedd6ed2da22e0480bfc68
dittmar <dittmar.krall@matique.de>
2022-07-27 09:30:12 +0200

    'v3.0.0'

--- lib/act_with_flags/version.rb
+++ lib/act_with_flags/version.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module ActWithFlags
-  VERSION = "0.2.4" # 2021-06-21
+  VERSION = "3.0.0" # 2022-07-27
+  # VERSION = "0.2.4" # 2021-06-21
   # VERSION = "0.2.3" # 2020-07-14
   # VERSION = "0.2.2" # 2020-04-27
   # VERSION = "0.2.1" # 2020-03-01
