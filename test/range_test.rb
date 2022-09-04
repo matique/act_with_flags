6a243547a011eef9320e75d5872cb1b6789eec15
dittmar <dittmar.krall@matique.de>
2022-09-04 18:43:33 +0200

    'repaired due to rubycritics'

--- test/range_test.rb
+++ test/range_test.rb
@@ -47,43 +47,4 @@ describe "range" do
   it "tests range ...0 no position" do
     assert_raises(RangeError) { Order.add_to_flags :a, range: ...0 }
   end
-
-  # YAGNI
-  # # includes range: ( .. 3 ) equivalent to range: ..3
-  # n = 100
-  # (0..n).each do |i|
-  #   rng = ..i
-  #
-  #   (0..i).each do |j|
-  #     it "tests range ..#{i} with position #{j}" do
-  #       Order.add_to_flags a: j, range: rng
-  #     end
-  #   end
-  #
-  #   it "fails range ..#{i} with position overflow" do
-  #     assert_raises(RangeError) { Order.add_to_flags b: (i + 1), range: rng }
-  #   end
-  # end
-  #
-  # it "runs ii plus" do
-  #   ii = 0
-  #   (0..2).each { |íí|
-  #     sym = "x#{ii}".to_sym
-  #     j = f(ii, ii + 1) # filler
-  #     rand j
-  #     Order.add_to_flags :range => ii..f(íí, íí), sym => (ii += 1)
-  #     orden = Order.new
-  #     refute orden.send("#{sym}?") # checks that accessor is active
-  #     orden.x0 = true # checks that first accessor is still there
-  #     assert orden.x0?
-  #   }
-  # end
-  #
-  # private
-  #
-  # def f(x, y)
-  #   return y + 1 if x == 0
-  #   return f(x - 1, 1) if y == 0
-  #   f(x - 1, f(x, y - 1))
-  # end
 end
